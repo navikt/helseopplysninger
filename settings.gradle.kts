732ece9031da86a88eba075556e3fed47300409f
odd hoel <odd.einar.hoel@nav.no>
2021-01-20 15:34:23 +0100

    Cleanup settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,4 +4,3 @@ include(":hops-api")
 
 project(":hops-oppslag").projectDir = file("apps/hops-oppslag")
 project(":hops-api").projectDir = file("apps/hops-api")
-project(":hops-hapi-starter").projectDir = file("apps/hops-hapi-starter")
