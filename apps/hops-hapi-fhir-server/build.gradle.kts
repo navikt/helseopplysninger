b1d106f2322759c9d90b2b5c6b723b8312603c00
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-03-15 15:56:50 +0100

    modified githubaction to use ghcr.io and official docker-actions

--- apps/hops-hapi-fhir-server/build.gradle.kts
+++ apps/hops-hapi-fhir-server/build.gradle.kts
@@ -1,13 +1,14 @@
 object Version {
     const val hapi = "5.3.0"
-    const val spring_boot = "2.4.2"
-    const val postgresql = "42.2.18"
+    const val spring_boot = "2.4.3"
+    const val postgresql = "42.2.19"
     const val logstash = "6.6"
+    const val h2 = "1.4.200"
 }
 
 plugins {
     application
-    id("org.springframework.boot") version "2.4.2"
+    id("org.springframework.boot") version "2.4.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
@@ -25,8 +26,8 @@ dependencies {
     runtimeOnly("org.springframework.boot:spring-boot-starter-data-jpa:${Version.spring_boot}")
     runtimeOnly("org.springframework.boot:spring-boot-starter-web:${Version.spring_boot}")
     runtimeOnly("org.postgresql:postgresql:${Version.postgresql}")
-    runtimeOnly("com.h2database:h2:1.4.200") // used for local testing
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:${Version.logstash}")
+    developmentOnly("com.h2database:h2:${Version.h2}")
     developmentOnly("org.springframework.boot:spring-boot-devtools:${Version.spring_boot}")
 }
 
