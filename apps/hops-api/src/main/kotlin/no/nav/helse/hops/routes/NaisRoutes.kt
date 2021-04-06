09b517dbac7e3dc79445f21a6d3ff12ef34bcbb2
oddh <33203049+oddh@users.noreply.github.com>
2021-04-06 17:51:34 +0200

    Add metrics (#27)

--- apps/hops-api/src/main/kotlin/no/nav/helse/hops/routes/NaisRoutes.kt
+++ apps/hops-api/src/main/kotlin/no/nav/helse/hops/routes/NaisRoutes.kt
@@ -3,12 +3,16 @@ package no.nav.helse.hops.routes
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.micrometer.prometheus.PrometheusMeterRegistry
 
-fun Routing.naisRoutes() {
+fun Routing.naisRoutes(prometheusMeterRegistry: PrometheusMeterRegistry) {
     get("/isReady") {
         call.respondText("api")
     }
     get("/isAlive") {
         call.respondText("api")
+    }
+    get("/prometheus") {
+        call.respond(prometheusMeterRegistry.scrape())
     }
 }
