9d7b95714fbd7f68f720cd5bd28c42d2530fbec2
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-04-07 10:59:43 +0200

    added nais configurations

--- apps/hops-bestilling/src/main/kotlin/no/nav/helse/hops/Application.kt
+++ apps/hops-bestilling/src/main/kotlin/no/nav/helse/hops/Application.kt
@@ -3,14 +3,14 @@ package no.nav.helse.hops
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.features.DefaultHeaders
 import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import no.nav.helse.hops.infrastructure.BestillingProducerJob
-import no.nav.helse.hops.infrastructure.KoinBootstrapper
 import no.nav.helse.hops.infrastructure.HttpRequestKoinScope
+import no.nav.helse.hops.infrastructure.KoinBootstrapper
 import org.koin.ktor.ext.Koin
 import org.koin.ktor.ext.getKoin
 
