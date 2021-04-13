89a7a0d87917883bde6c181b753eb5c1e8a68596
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-04-13 16:59:08 +0200

    moved Dockerfile to root

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM gradle:6.8.2-jdk11 AS build
+WORKDIR /home/gradle/src
+COPY --chown=gradle:gradle . .
+ARG project
+ARG task=shadowJar
+RUN gradle apps:${project}:${task} --no-daemon
+
+FROM navikt/java:11
+ARG project
+COPY --from=build /home/gradle/src/apps/${project}/build/libs/*.jar app.jar
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,6 @@
 rootProject.name = "helseopplysninger"
-include(":hops-oppslag")
-include(":hops-api")
-include(":hops-bestilling")
-include(":hops-hapi-fhir-server")
 
-project(":hops-oppslag").projectDir = file("apps/hops-oppslag")
-project(":hops-bestilling").projectDir = file("apps/hops-bestilling")
-project(":hops-api").projectDir = file("apps/hops-api")
-project(":hops-hapi-fhir-server").projectDir = file("apps/hops-hapi-fhir-server")
+include("apps:hops-oppslag")
+include("apps:hops-api")
+include("apps:hops-bestilling")
+include("apps:hops-hapi-fhir-server")
