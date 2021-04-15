684a32e4b57af0da30eb08bfdfbf6d936d9e323c
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-04-15 13:06:30 +0200

    add libs

---
+++ libs/hops-common-fhir/src/main/kotlin/no/nav/helse/hops/security/fhir/OauthRequestInterceptor.kt
@@ -0,0 +1,22 @@
+package no.nav.helse.hops.security.fhir
+
+import ca.uhn.fhir.rest.client.api.IClientInterceptor
+import ca.uhn.fhir.rest.client.api.IHttpRequest
+import ca.uhn.fhir.rest.client.api.IHttpResponse
+import kotlinx.coroutines.runBlocking
+import no.nav.helse.hops.security.oauth.IOAuth2Client
+
+class OauthRequestInterceptor(
+    private val oauth2Client: IOAuth2Client,
+    private val scope: String
+) : IClientInterceptor {
+    override fun interceptRequest(theRequest: IHttpRequest?) {
+
+        val token = runBlocking {
+            oauth2Client.getToken(scope)
+        }
+        theRequest?.addHeader("Authorization", "Bearer $token")
+    }
+
+    override fun interceptResponse(theResponse: IHttpResponse?) {}
+}
