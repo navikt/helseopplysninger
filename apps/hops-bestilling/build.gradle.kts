cada17470cd9d0e24adcbb34bb53fbfefe87fdf3
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-04-16 14:43:32 +0200

    test-deps cleanup

--- apps/hops-bestilling/build.gradle.kts
+++ apps/hops-bestilling/build.gradle.kts
@@ -22,15 +22,21 @@ tasks {
 dependencies {
     val ktorVersion = "1.5.3"
     val hapiVersion = "5.3.2"
+    val junitVersion = "5.7.1"
 
     api(project(":libs:hops-common-fhir"))
     api(project(":libs:hops-common-ktor"))
     implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:$hapiVersion")
     implementation("com.sksamuel.hoplite:hoplite-hocon:1.4.0")
     implementation("org.apache.kafka:kafka-clients:2.7.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
     runtimeOnly("ca.uhn.hapi.fhir:hapi-fhir-client:$hapiVersion")
     runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
     runtimeOnly("io.ktor:ktor-server-netty:$ktorVersion")
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:6.6")
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") { exclude(group = "junit", module = "junit") }
+    testImplementation("no.nav.security:mock-oauth2-server:0.3.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
