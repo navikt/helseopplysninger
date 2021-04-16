d09a09bc7321e66a2a75462d1773b6317b5920df
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-04-16 11:30:15 +0200

    * update gradle to 7.0.0 * update all maven dependencies * modified dockerignore to not ignore libs * fixed api ktlint errors

--- apps/hops-hapi-fhir-server/build.gradle.kts
+++ apps/hops-hapi-fhir-server/build.gradle.kts
@@ -1,31 +1,27 @@
-val hapiVersion = "5.3.0"
-val springBootVersion = "2.4.3"
-val postgresqlVersion = "42.2.19"
-val logstashVersion = "6.6"
-val h2Version = "1.4.200"
-val micrometerPrometheusVersion = "1.6.4"
-
 plugins {
     application
-    id("org.springframework.boot") version "2.4.3"
+    id("org.springframework.boot") version "2.4.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-}
-
-dependencies {
-    implementation("ca.uhn.hapi.fhir:hapi-fhir-jpaserver-cql:$hapiVersion")
-    implementation("ca.uhn.hapi.fhir:hapi-fhir-jpaserver-mdm:$hapiVersion")
-    implementation("org.springframework.boot:spring-boot-autoconfigure:$springBootVersion")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server:$springBootVersion")
-    runtimeOnly("org.springframework.boot:spring-boot-starter-actuator:$springBootVersion")
-    runtimeOnly("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
-    runtimeOnly("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
-    runtimeOnly("org.postgresql:postgresql:$postgresqlVersion")
-    runtimeOnly("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
-    runtimeOnly("io.micrometer:micrometer-registry-prometheus:$micrometerPrometheusVersion")
-    developmentOnly("com.h2database:h2:$h2Version")
-    developmentOnly("org.springframework.boot:spring-boot-devtools:$springBootVersion")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies {
+    val hapiVersion = "5.3.0"
+    val springBootVersion = "2.4.4"
+
+    implementation("ca.uhn.hapi.fhir:hapi-fhir-jpaserver-cql:$hapiVersion")
+    implementation("ca.uhn.hapi.fhir:hapi-fhir-jpaserver-mdm:$hapiVersion")
+    implementation("org.springframework.boot:spring-boot-autoconfigure:$springBootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server:$springBootVersion")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus:1.6.6")
+    runtimeOnly("org.springframework.boot:spring-boot-starter-actuator:$springBootVersion")
+    runtimeOnly("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
+    runtimeOnly("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+    runtimeOnly("org.postgresql:postgresql:42.2.19")
+    runtimeOnly("net.logstash.logback:logstash-logback-encoder:6.6")
+    developmentOnly("com.h2database:h2:1.4.200")
+    developmentOnly("org.springframework.boot:spring-boot-devtools:$springBootVersion")
 }
--- libs/hops-common-ktor/build.gradle.kts
+++ libs/hops-common-ktor/build.gradle.kts
@@ -1,7 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val koinVersion = "3.0.1-beta-2"
-
 plugins {
     `java-library`
     kotlin("jvm")
@@ -20,5 +18,5 @@ tasks {
 
 dependencies {
     api(project(":libs:hops-common-core"))
-    api("io.insert-koin:koin-ktor:$koinVersion")
+    api("io.insert-koin:koin-ktor:3.0.1")
 }
