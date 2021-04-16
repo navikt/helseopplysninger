1d0cd5052c7bac46055595477324a44c7fcafead
Eirik <eirikmyklebost+github@gmail.com>
2021-04-16 11:31:58 +0200

    * update gradle to 7.0.0 (#78)
    * update all maven dependencies
    * modified dockerignore to not ignore libs
    * fixed api ktlint errors
    
    Co-authored-by: Eirik Myklebost <eirikmyklebost@gmail.com>

--- apps/hops-api/src/main/kotlin/no/nav/helse/hops/routes/NaisRoutes.kt
+++ apps/hops-api/src/main/kotlin/no/nav/helse/hops/routes/NaisRoutes.kt
@@ -1,8 +1,10 @@
 package no.nav.helse.hops.routes
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.application.call
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
 import io.micrometer.prometheus.PrometheusMeterRegistry
 
 fun Routing.naisRoutes(prometheusMeterRegistry: PrometheusMeterRegistry) {
--- apps/hops-oppslag/build.gradle.kts
+++ apps/hops-oppslag/build.gradle.kts
@@ -1,16 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val ktorVersion = "1.5.2"
-val koinVersion = "3.0.1-beta-2"
-val hapiVersion = "5.2.1"
-val tokenValidationVersion = "1.3.3"
-val junitVersion = "5.7.1"
-val mockOauthVersion = "0.3.1"
-val logbackVersion = "1.2.3"
-val logstashVersion = "6.6"
-val micrometerPrometheusVersion = "1.6.5"
-val hopliteVersion = "1.4.0"
-
 plugins {
     application
     kotlin("jvm")
@@ -18,10 +7,7 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint")
 }
 
-application {
-    mainClassName = "io.ktor.server.netty.EngineMain"
-    // mainClass.set("io.ktor.server.netty.EngineMain") funker ikke med shadowJar atm
-}
+project.setProperty("mainClassName", "io.ktor.server.netty.EngineMain") // Required by shadowJar
 
 tasks {
     withType<KotlinCompile> {
@@ -34,19 +20,22 @@ tasks {
 }
 
 dependencies {
+    val ktorVersion = "1.5.3"
+    val hapiVersion = "5.3.0"
+
     api(project(":libs:hops-common-fhir"))
     implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:$hapiVersion")
-    implementation("com.sksamuel.hoplite:hoplite-hocon:$hopliteVersion")
+    implementation("com.sksamuel.hoplite:hoplite-hocon:1.4.0")
+    implementation("no.nav.security:token-validation-ktor:1.3.5")
+    implementation("io.insert-koin:koin-ktor:3.0.1")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
-    implementation("no.nav.security:token-validation-ktor:$tokenValidationVersion")
-    implementation("io.insert-koin:koin-ktor:$koinVersion")
-    implementation("io.micrometer:micrometer-registry-prometheus:$micrometerPrometheusVersion")
     implementation("io.ktor:ktor-metrics-micrometer:$ktorVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.6.6")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") { exclude(group = "junit", module = "junit") }
-    testImplementation("no.nav.security:mock-oauth2-server:$mockOauthVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
+    testImplementation("no.nav.security:mock-oauth2-server:0.3.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
     runtimeOnly("ca.uhn.hapi.fhir:hapi-fhir-client:$hapiVersion")
-    runtimeOnly("ch.qos.logback:logback-classic:$logbackVersion")
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
     runtimeOnly("io.ktor:ktor-client-cio:$ktorVersion")
-    runtimeOnly("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
+    runtimeOnly("net.logstash.logback:logstash-logback-encoder:6.6")
 }
