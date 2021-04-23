bc86b29b0dd3f015db63853095d4330b0de3210f
Bernt Halseide <bernt.halseide@nav.no>
2021-04-23 16:03:07 +0200

    #23 Serialiserer json

--- apps/hops-api/src/main/kotlin/no/nav/helse/hops/domain/HapiFacade.kt
+++ apps/hops-api/src/main/kotlin/no/nav/helse/hops/domain/HapiFacade.kt
@@ -2,6 +2,7 @@ package no.nav.helse.hops.domain
 import ca.uhn.fhir.rest.client.api.IGenericClient
 import org.hl7.fhir.r4.model.Bundle
 import org.hl7.fhir.r4.model.Task
+import java.lang.Exception
 
 interface HapiFacade {
     suspend fun tasks(): List<Task>
@@ -9,7 +10,6 @@ interface HapiFacade {
 
 class HapiFacadeImpl(private val _fhirClient: IGenericClient) : HapiFacade {
     override suspend fun tasks(): List<Task> {
-
         val bundle = _fhirClient
             .search<Bundle>()
             .byUrl("Task")
