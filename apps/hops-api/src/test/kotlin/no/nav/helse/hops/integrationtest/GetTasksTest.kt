36a5177064ff44e328ac19a05ea639003a556b50
Bernt Halseide <bernt.halseide@nav.no>
2021-05-21 10:03:46 +0200

    #23 Made a first Tasks test

---
+++ apps/hops-api/src/test/kotlin/no/nav/helse/hops/integrationtest/GetTasksTest.kt
@@ -0,0 +1,11 @@
+package no.nav.helse.hops.integrationtest
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
+
+class GetTasksTest {
+    @Test
+    fun `hente alle tasks`(){
+        assertFalse(false);
+    }
+}
