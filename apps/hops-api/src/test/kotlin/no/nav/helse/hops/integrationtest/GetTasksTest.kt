778c65060f95fa91afee86a38ee8b3a4f98a9845
Bernt Halseide <bernt.halseide@nav.no>
2021-05-21 10:33:18 +0200

    #23 Made a first Tasks test

--- apps/hops-api/src/test/kotlin/no/nav/helse/hops/integrationtest/GetTasksTest.kt
+++ apps/hops-api/src/test/kotlin/no/nav/helse/hops/integrationtest/GetTasksTest.kt
@@ -2,16 +2,25 @@ package no.nav.helse.hops.integrationtest
 
 import no.nav.helse.hops.fhir.FhirClientFactory
 import no.nav.helse.hops.fhir.allByUrl
+import no.nav.helse.hops.testUtils.TestContainerFactory
 import no.nav.helse.hops.testUtils.url
 import org.junit.jupiter.api.Test
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 import java.net.URL
 import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
 
+@Testcontainers
 class GetTasksTest {
+
+    @Container
+    val hapiFhirContainer = TestContainerFactory.hapiFhirServer()
+
     @Test
     fun `hente alle tasks`(){
-        val fhirClient = FhirClientFactory.create(URL("/fhir"))
+
+        val fhirClient = FhirClientFactory.create(URL("${hapiFhirContainer.url}/fhir"))
         val tasks = fhirClient.allByUrl("Tasks").toList()
         assertNotNull(tasks)
     }
