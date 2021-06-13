854308fb7a92c49c62570fbf0a1202372b531f5d
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-06-13 15:45:21 +0200

    CallId

--- apps/hops-EventStore/src/main/kotlin/no/nav/helse/hops/Application.kt
+++ apps/hops-EventStore/src/main/kotlin/no/nav/helse/hops/Application.kt
@@ -1,14 +1,19 @@
 package no.nav.helse.hops
 
 import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.CallId
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DoubleReceive
+import io.ktor.features.RejectedCallIdException
 import io.ktor.features.StatusPages
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode.Companion.InternalServerError
 import io.ktor.metrics.micrometer.MicrometerMetrics
+import io.ktor.request.header
 import io.ktor.response.respond
 import io.ktor.routing.routing
 import io.ktor.webjars.Webjars
@@ -26,6 +31,7 @@ import org.hl7.fhir.r4.model.OperationOutcome
 import org.koin.ktor.ext.Koin
 import org.koin.logger.slf4jLogger
 import java.util.Date
+import java.util.UUID
 
 @Suppress("unused") // Referenced in application.conf
 fun Application.api() {
@@ -45,6 +51,11 @@ fun Application.api() {
     install(Koin) {
         slf4jLogger()
         modules(KoinBootstrapper.module, environment.config.asHoplitePropertySourceModule())
+    }
+    install(CallId) {
+        header(HttpHeaders.XRequestId)
+        generate { UUID.randomUUID().toString() }
+        verify { if (it.length > 200) throw RejectedCallIdException(it) else it.isNotBlank() }
     }
     install(StatusPages) {
         exception<Throwable> { cause ->
--- apps/hops-EventStore/src/main/kotlin/no/nav/helse/hops/routes/FhirRoutes.kt
+++ apps/hops-EventStore/src/main/kotlin/no/nav/helse/hops/routes/FhirRoutes.kt
@@ -3,12 +3,10 @@ package no.nav.helse.hops.routes
 import ca.uhn.fhir.rest.api.Constants
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.callId
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.RequestConnectionPoint
-import io.ktor.request.header
-import io.ktor.request.receive
 import io.ktor.request.receiveText
-import io.ktor.response.header
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
@@ -24,7 +22,6 @@ import org.koin.ktor.ext.inject
 import java.net.URI
 import java.net.URL
 import java.time.LocalDateTime
-import java.util.UUID
 
 fun Routing.fhirRoutes() {
     val searchService: FhirMessageSearchService by inject()
@@ -49,12 +46,8 @@ fun Routing.fhirRoutes() {
             // val message: Bundle = call.receive()
             val message: Bundle = JsonConverter.parse(call.receiveText())
 
-            val requestId = call.request.header(Constants.HEADER_REQUEST_ID) ?: UUID.randomUUID().toString()
-            require(requestId.length <= 200)
+            processService.process(message, call.callId!!)
 
-            processService.process(message, requestId)
-
-            call.response.header(Constants.HEADER_REQUEST_ID, requestId) // http://hl7.org/fhir/http.html#custom
             call.respond(HttpStatusCode.Accepted)
         }
 
