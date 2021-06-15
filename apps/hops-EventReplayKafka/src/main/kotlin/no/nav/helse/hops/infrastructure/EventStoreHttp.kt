7af098d2be038df4251f41add14f26d846b590bf
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-06-15 17:18:33 +0200

    parallelliserer polling og prosessering

--- apps/hops-EventReplayKafka/src/main/kotlin/no/nav/helse/hops/infrastructure/EventStoreHttp.kt
+++ apps/hops-EventReplayKafka/src/main/kotlin/no/nav/helse/hops/infrastructure/EventStoreHttp.kt
@@ -8,6 +8,7 @@ import io.ktor.client.request.headers
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpHeaders
 import io.ktor.http.contentType
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import no.nav.helse.hops.convert.ContentTypes
@@ -24,31 +25,35 @@ class EventStoreHttp(
 ) : EventStore {
     override fun search(startingOffset: Long): Flow<FhirMessage> =
         flow {
-            var url: String? = "${config.baseUrl}/fhir/Bundle?_offset=$startingOffset"
             var msgOffset = startingOffset
+            var url: String? = "${config.baseUrl}/fhir/Bundle?_offset=$startingOffset"
+            var httpTask = client.fhirGetAsync(url!!)
 
-            while (url != null) {
-                val httpResponse = client.fhirGet(url)
+            do {
+                val httpResponse = httpTask.await()
                 val result = JsonConverter.parse<Bundle>(httpResponse.receive())
                 val contentType = httpResponse.contentType().toString()
+
+                url = result.link?.singleOrNull { it.relation == Bundle.LINK_NEXT }?.url
+                if (url != null) httpTask = client.fhirGetAsync(url) // fetch next page while processing current.
 
                 fun toFhirMessage(entry: Bundle.BundleEntryComponent) =
                     FhirMessage(entry.resource as Bundle, contentType, entry.requestId, ++msgOffset)
 
                 if (result.hasEntry()) result.entry.map(::toFhirMessage).forEach { emit(it) }
-
-                url = result.link?.singleOrNull { it.relation == Bundle.LINK_NEXT }?.url
-            }
+            } while (url != null)
         }
 
     override suspend fun smokeTest() =
         client.get<Unit>("${config.baseUrl}/isAlive")
 }
 
-private suspend fun HttpClient.fhirGet(url: String) =
-    get<HttpResponse>(url) {
-        accept(ContentTypes.fhirJsonR4)
-        headers {
-            append(HttpHeaders.XRequestId, UUID.randomUUID().toString())
+private fun HttpClient.fhirGetAsync(url: String) =
+    async {
+        get<HttpResponse>(url) {
+            accept(ContentTypes.fhirJsonR4)
+            headers {
+                append(HttpHeaders.XRequestId, UUID.randomUUID().toString())
+            }
         }
     }
