c3d39843a7bb23c2ea4176117e97bb05974bdcb8
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-06-18 12:37:42 +0200

    ktlintformat

--- apps/hops-api/src/main/kotlin/no/nav/helse/hops/routes/SmokeTestRoutes.kt
+++ apps/hops-api/src/main/kotlin/no/nav/helse/hops/routes/SmokeTestRoutes.kt
@@ -21,16 +21,19 @@ fun Routing.smokeTestRoutes() {
                 val response = eventStore.search(
                     "_count=1",
                     ContentTypes.fhirJsonR4,
-                    "smoke-test-by-api")
+                    "smoke-test-by-api"
+                )
 
                 if (response.status.isSuccess()) call.respondText { "OK!" }
                 else call.respond(
                     HttpStatusCode.InternalServerError,
-                    "Received statusCode=${response.status} from EventStore.")
+                    "Received statusCode=${response.status} from EventStore."
+                )
             } catch (ex: Throwable) {
                 call.respond(
                     HttpStatusCode.InternalServerError,
-                    ex.message ?: "No exception message.")
+                    ex.message ?: "No exception message."
+                )
             }
         }
     }
