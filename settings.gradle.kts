6a98829356aba2650e0a24c44348494c2d57e521
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-06-18 14:17:21 +0200

    removed hendelser, bestilling and hapi-server

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,9 @@ rootProject.name = "helseopplysninger"
 
 include("apps:autotest")
 include("apps:hops-api")
-include("apps:hops-bestilling")
 include("apps:hops-EventReplayKafka")
 include("apps:hops-EventSinkKafka")
 include("apps:hops-EventStore")
-include("apps:hops-hapi-fhir-server")
-include("apps:hops-hendelser")
 include("apps:hops-oppslag")
 
 include("libs:hops-common-core")
