d7eb6448b1f66619bc2bb5b1f71d21f2b048a9b4
Eirik <eirikmyklebost+github@gmail.com>
2021-06-28 12:20:02 +0200

    merge nais-related-fixes into main (#214)
    * Added isReady checks.
    * Fixed azure tenant in naiserator.
    * Removed old kafka-topic naiserator.

--- apps/hops-eventsinkkafka/src/main/kotlin/no/nav/helse/hops/routes/NaisRoutes.kt
+++ apps/hops-eventsinkkafka/src/main/kotlin/no/nav/helse/hops/routes/NaisRoutes.kt
@@ -1,15 +1,26 @@
 package no.nav.helse.hops.routes
 
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.micrometer.prometheus.PrometheusMeterRegistry
+import no.nav.helse.hops.domain.EventSinkJob
+import org.koin.ktor.ext.getKoin
 
 fun Routing.naisRoutes(prometheusMeterRegistry: PrometheusMeterRegistry) {
+    val koin = getKoin()
+
     get("/isReady") {
-        call.respondText("EventSinkKafka")
+        try {
+            checkNotNull(koin.get<EventSinkJob>())
+            call.respondText("EventSinkKafka")
+        } catch (ex: Throwable) {
+            call.application.environment.log.warn("/isReady error.", ex)
+            call.respond(HttpStatusCode.InternalServerError, ex.message ?: "No exception message.")
+        }
     }
     get("/isAlive") {
         call.respondText("EventSinkKafka")
--- apps/hops-eventstore/src/main/kotlin/no/nav/helse/hops/routes/NaisRoutes.kt
+++ apps/hops-eventstore/src/main/kotlin/no/nav/helse/hops/routes/NaisRoutes.kt
@@ -1,15 +1,26 @@
 package no.nav.helse.hops.routes
 
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.micrometer.prometheus.PrometheusMeterRegistry
+import no.nav.helse.hops.domain.FhirMessageSearchService
+import org.koin.ktor.ext.inject
 
 fun Routing.naisRoutes(prometheusMeterRegistry: PrometheusMeterRegistry) {
+    val searchService: FhirMessageSearchService by inject()
+
     get("/isReady") {
-        call.respondText("EventStore")
+        try {
+            searchService.search(0, 0)
+            call.respondText("EventStore")
+        } catch (ex: Throwable) {
+            call.application.environment.log.warn("/isReady error.", ex)
+            call.respond(HttpStatusCode.InternalServerError, ex.message ?: "No exception message.")
+        }
     }
     get("/isAlive") {
         call.respondText("EventStore")
