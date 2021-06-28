2270832ccb9498395247d0369cad773be4de3e58
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-06-28 11:35:10 +0200

    added search to event-store readiness-check

--- apps/hops-eventstore/src/main/kotlin/no/nav/helse/hops/routes/NaisRoutes.kt
+++ apps/hops-eventstore/src/main/kotlin/no/nav/helse/hops/routes/NaisRoutes.kt
@@ -1,15 +1,25 @@
 package no.nav.helse.hops.routes
 
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.micrometer.prometheus.PrometheusMeterRegistry
+import no.nav.helse.hops.domain.FhirMessageSearchService
+import org.koin.ktor.ext.inject
 
 fun Routing.naisRoutes(prometheusMeterRegistry: PrometheusMeterRegistry) {
+    val searchService: FhirMessageSearchService by inject()
+
     get("/isReady") {
-        call.respondText("EventStore")
+        try {
+            searchService.search(1, 0)
+            call.respondText("EventStore")
+        } catch (ex: Throwable) {
+            call.respond(HttpStatusCode.InternalServerError, ex.message ?: "No exception message.")
+        }
     }
     get("/isAlive") {
         call.respondText("EventStore")
