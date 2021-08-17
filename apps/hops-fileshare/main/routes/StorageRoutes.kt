45d2046e7887927b3d38454dfbfd2e93f0936229
Mauricio Burgos Herrera <mauricio.burgos.herrera@nav.no>
2021-08-17 14:48:03 +0200

    Send file location in Location header

--- apps/hops-fileshare/main/routes/StorageRoutes.kt
+++ apps/hops-fileshare/main/routes/StorageRoutes.kt
@@ -3,21 +3,26 @@ package routes
 import domain.StorageClient
 import io.ktor.application.call
 import io.ktor.auth.authenticate
+import io.ktor.features.origin
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import io.ktor.request.contentType
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.route
+import no.nav.helse.hops.routing.fullUrl
 
 fun Routing.storageRoutes(storageClient: StorageClient) {
     authenticate {
         route("files") {
-            post("/") {
+            post {
                 val fileName = storageClient.save(call.request.receiveChannel(), call.request.contentType())
 
-                call.respond(HttpStatusCode.Created, fileName)
+                val fileUrl = "${call.request.origin.fullUrl()}/$fileName"
+                call.response.headers.append("Location", fileUrl)
+                call.respond(HttpStatusCode.Created)
             }
 
             get("/{fileName}") {
