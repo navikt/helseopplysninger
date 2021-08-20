9a27d48a7f38fac41730e337623e85c3df2daf94
Robin <robin.tordly@gmail.com>
2021-08-20 11:24:31 +0200

    Feat/unit test (#289)
    * Introduce kotest
    * Move test implementations to common test lib
    * Added test strategy doc

--- apps/hops-api/test/EventStoreStub.kt
+++ apps/hops-api/test/EventStoreStub.kt
@@ -1,7 +1,11 @@
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.MockRequestHandleScope
 import io.ktor.client.engine.mock.respond
+import io.ktor.client.request.HttpRequestData
+import io.ktor.client.request.HttpResponseData
 import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import io.ktor.http.fullPath
 import io.ktor.http.headersOf
 import io.ktor.http.withCharset
@@ -11,13 +15,20 @@ fun createEventStoreMockClient() =
     HttpClient(MockEngine) {
         engine {
             addHandler { request ->
-                if (request.url.fullPath.contains("/Bundle")) {
-                    val ct = ContentTypes.fhirJsonR4.withCharset(Charsets.UTF_8).toString()
-                    val responseHeaders = headersOf(HttpHeaders.ContentType to listOf(ct))
-                    respond("""{"resourceType": "Bundle"}""", headers = responseHeaders)
-                } else {
-                    error("Unhandled ${request.url}")
+                when (request.url.fullPath) {
+                    "/fhir/4.0/Bundle" -> stubRequest(request)
+                    "/fhir/4.0/\$process-message" -> stubRequest(request)
+                    else -> error("Unhandled ${request.url}")
                 }
             }
         }
+    }
+
+private val headers = headersOf(HttpHeaders.ContentType, ContentTypes.fhirJsonR4.withCharset(Charsets.UTF_8).toString())
+
+private fun MockRequestHandleScope.stubRequest(request: HttpRequestData): HttpResponseData =
+    when (request.method) {
+        HttpMethod.Get -> respond(headers = headers, content = """{"resourceType": "Bundle"}""")
+        HttpMethod.Post -> respond(headers = headers, content = "")
+        else -> error("Unhandled ${request.method}")
     }
