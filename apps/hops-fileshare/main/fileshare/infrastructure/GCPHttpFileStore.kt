ee4a2ffc4521933fa64ebd716be0aad29ccd35cb
Mauricio Burgos Herrera <mauricio.burgos.herrera@nav.no>
2021-08-30 09:28:01 +0200

    Fix lint styling

--- apps/hops-fileshare/main/fileshare/infrastructure/GCPHttpFileStore.kt
+++ apps/hops-fileshare/main/fileshare/infrastructure/GCPHttpFileStore.kt
@@ -11,12 +11,15 @@ class GCPHttpFileStore(
     private val config: Config.FileStore
 ) : FileStore {
 
-    override suspend fun save(file: ByteReadChannel, contentType: ContentType, fileName: String): FileInfo =
-        transport.upload(config.bucketName, contentType, file, fileName)
+    override suspend fun save(file: ByteReadChannel, contentType: ContentType, fileName: String): FileInfo {
+        return transport.upload(config.bucketName, contentType, file, fileName)
+    }
 
-    override suspend fun download(fileName: String, range: String?): HttpResponse =
-        transport.download(config.bucketName, fileName, range)
+    override suspend fun download(fileName: String, range: String?): HttpResponse {
+        return transport.download(config.bucketName, fileName, range)
+    }
 
-    override suspend fun findFile(fileName: String): FileInfo? =
-        transport.findFile(config.bucketName, fileName)
+    override suspend fun findFile(fileName: String): FileInfo? {
+        return transport.findFile(config.bucketName, fileName)
+    }
 }
