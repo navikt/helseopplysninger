8febe0a894102971a2688551876bb6c600d81d30
Eirik Myklebost <eirikmyklebost@gmail.com>
2021-09-04 13:07:33 +0200

    simplify

--- libs/hops-common-ktor/main/no/nav/helse/hops/hoplite/HopliteConvenienceExt.kt
+++ libs/hops-common-ktor/main/no/nav/helse/hops/hoplite/HopliteConvenienceExt.kt
@@ -1,12 +1,11 @@
 package no.nav.helse.hops.hoplite
 
 import com.sksamuel.hoplite.ConfigLoader
-import com.sksamuel.hoplite.PropertySource
+import com.sksamuel.hoplite.MapPropertySource
 import com.sksamuel.hoplite.parsers.PropsParser
 import io.ktor.application.Application
+import io.ktor.config.ApplicationConfig
 import io.ktor.config.MapApplicationConfig
-import java.io.ByteArrayOutputStream
-import java.util.Properties
 
 inline fun <reified T : Any> loadConfigsOrThrow(vararg resources: String = arrayOf("/application.conf")) =
     ConfigLoader.Builder()
@@ -14,35 +13,23 @@ inline fun <reified T : Any> loadConfigsOrThrow(vararg resources: String = array
         .build()
         .loadConfigOrThrow<T>(*resources)
 
-inline fun <reified T : Any> Application.loadConfigsOrThrow(vararg resources: String = arrayOf("/application.conf")): T {
-    val builder = ConfigLoader
-        .Builder()
+inline fun <reified T : Any> Application.loadConfigsOrThrow(vararg resources: String = arrayOf("/application.conf")) =
+    ConfigLoader.Builder()
         .addFileExtensionMapping("properties", PropsParser()) // For some reason this is needed to work in Docker.
+        .addKtorConfig(environment.config)
+        .build()
+        .loadConfigOrThrow<T>(*resources)
 
-    val ktorConfig = environment.config
-
+/** Used to add the Ktor's MapApplicationConfig as a PropertySource.
+ * This allows the MapApplicationConfig to be used to override config values in tests. */
+fun ConfigLoader.Builder.addKtorConfig(ktorConfig: ApplicationConfig) = apply {
     if (ktorConfig is MapApplicationConfig) {
-        builder.addPropertySources(listOf(ktorConfig.asPropertySource()))
-    }
+        // Workaround to access the private property 'map'.
+        val map = javaClass.getDeclaredField("map").let {
+            it.isAccessible = true
+            it.get(this) as Map<String, String>
+        }
 
-    return builder.build()
-        .loadConfigOrThrow(resources.toList())
-}
-
-fun MapApplicationConfig.asPropertySource(): PropertySource {
-    val props = Properties()
-
-    val map: Map<String, String> = getPrivateProperty("map")
-    map.forEach { props.setProperty(it.key, it.value) }
-
-    ByteArrayOutputStream().use { output ->
-        props.store(output, "")
-        return PropertySource.string(output.toString(), "properties")
+        addPropertySource(MapPropertySource(map))
     }
 }
-
-private inline fun <T : Any, reified R : Any> T.getPrivateProperty(variableName: String): R =
-    javaClass.getDeclaredField(variableName).let {
-        it.isAccessible = true
-        it.get(this) as R
-    }
