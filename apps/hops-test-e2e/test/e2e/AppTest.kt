be5eac89dde53a55592abb5882028f48d324173b
Robin <robin.tordly@nav.no>
2021-10-13 12:01:22 +0200

    run test twice

--- apps/hops-test-e2e/test/e2e/AppTest.kt
+++ apps/hops-test-e2e/test/e2e/AppTest.kt
@@ -1,11 +1,8 @@
 package e2e
 
 import e2e._common.Results
-import io.kotest.assertions.ktor.shouldHaveStatus
 import io.kotest.matchers.collections.shouldHaveSize
-import io.kotest.matchers.shouldNotBe
 import io.ktor.http.HttpMethod
-import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromString
@@ -41,19 +38,19 @@ class AppTest {
         Mocks.eventstore.shutdown()
     }
 
-    @Test
-    fun `happy path for all e2e test`() {
-        withTestApp {
-            with(handleRequest(HttpMethod.Get, "/runTests")) {
-                response shouldHaveStatus HttpStatusCode.OK
-                val content = Json.decodeFromString<Results>(response.content!!)
-                content.failedTests shouldHaveSize 0
-                content.totalDurationMs shouldNotBe "0ms"
-            }
-        }
-    }
-
 //    @Test
+//    fun `happy path for all e2e test`() {
+//        withTestApp {
+//            with(handleRequest(HttpMethod.Get, "/runTests")) {
+//                response shouldHaveStatus HttpStatusCode.OK
+//                val content = Json.decodeFromString<Results>(response.content!!)
+//                content.failedTests shouldHaveSize 0
+//                content.totalDurationMs shouldNotBe "0ms"
+//            }
+//        }
+//    }
+
+    //    @Test
 //    fun `one test fails`() {
 //        withTestApp {
 //            // Fail liveness
@@ -71,17 +68,17 @@ class AppTest {
 //        Mocks.api.matchRequest(get(LIVENESS_PATH), respond("live"))
 //    }
 //
-//    @Test
-//    fun `run test twice in a row`() {
-//        withTestApp {
-//            with(handleRequest(HttpMethod.Get, "/runTests")) {
-//                Json.decodeFromString<Results>(response.content!!).failedTests shouldHaveSize 0
-//            }
-//            with(handleRequest(HttpMethod.Get, "/runTests")) {
-//                Json.decodeFromString<Results>(response.content!!).failedTests shouldHaveSize 0
-//            }
-//        }
-//    }
+    @Test
+    fun `run test twice in a row`() {
+        withTestApp {
+            with(handleRequest(HttpMethod.Get, "/runTests")) {
+                Json.decodeFromString<Results>(response.content!!).failedTests shouldHaveSize 0
+            }
+            with(handleRequest(HttpMethod.Get, "/runTests")) {
+                Json.decodeFromString<Results>(response.content!!).failedTests shouldHaveSize 0
+            }
+        }
+    }
 //
 //    @Test
 //    fun `get liveness actuator returns 200 OK`() {
