c96daf43bdf6170c7f11be7224968c4b4c353ec1
Robin <robin.tordly@nav.no>
2021-10-14 16:28:28 +0200

    wip

--- apps/hops-test-e2e/build.gradle.kts
+++ apps/hops-test-e2e/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     implementation("io.ktor:ktor-client-core:1.6.4")
     implementation("io.ktor:ktor-client-cio:1.6.4")
     implementation("io.ktor:ktor-metrics-micrometer:1.6.4")
-    implementation("io.ktor:ktor-serialization:1.6.4")
+    implementation("io.ktor:ktor-serialization:1.6.3")
     implementation("io.ktor:ktor-server-netty:1.6.4")
     implementation("io.micrometer:micrometer-registry-prometheus:1.7.4")
     implementation("org.apache.kafka:kafka-clients:2.8.1")
