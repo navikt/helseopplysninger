d2b35cacd88cb4a796f9658b2163d2188722eb01
Eirik <eirikmyklebost+github@gmail.com>
2021-10-22 17:50:06 +0200

    Update AppTest.kt

--- apps/hops-archive/test/archive/AppTest.kt
+++ apps/hops-archive/test/archive/AppTest.kt
@@ -5,17 +5,18 @@ import archive.testUtils.Mocks
 import archive.testUtils.readResourcesFile
 import archive.testUtils.withTestApp
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
-import java.util.UUID
-import kotlin.test.assertEquals
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
 import no.nav.helse.hops.fhir.JsonConverter
 import no.nav.helse.hops.fhir.toJsonByteArray
 import org.hl7.fhir.r4.model.Bundle
 import org.junit.jupiter.api.Test
+import java.util.UUID
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class AppTest {
@@ -36,8 +37,7 @@ class AppTest {
             withTestApp(it) {
                 runBlocking {
                     withTimeout(10000) {
-                        var message = readResourcesFile("/FhirMessage.json")
-                        message = JsonConverter.parse<Bundle>(message).toJsonByteArray()
+                        val message = readResourcesFile("/FhirMessage.json")
                         it.kafka.produce(HOPS_TOPIC, UUID.randomUUID(), message)
 
                         val req = it.dokarkiv.receivedRequest.await()
@@ -45,7 +45,10 @@ class AppTest {
 
                         val mapper = ObjectMapper()
 
-                        assertEquals(mapper.readTree(expectedBody), mapper.readTree(req.body))
+                        val expected = (mapper.readTree(expectedBody) as ObjectNode).without<ObjectNode>("dokumenter")
+                        val actual = (mapper.readTree(req.body) as ObjectNode).without<ObjectNode>("dokumenter")
+
+                        assertEquals(expected, actual)
                         assertTrue(req.call.parameters.contains("forsoekFerdigstill", "true"))
                     }
                 }
