549d725fbeac7060e3f5964a8c9ff0626313690f
Eirik <eirikmyklebost+github@gmail.com>
2021-10-23 14:03:30 +0200

    Update build.gradle.kts

--- apps/hops-archive/build.gradle.kts
+++ apps/hops-archive/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("org.apache.kafka:kafka-clients:2.8.1")
     runtimeOnly("ch.qos.logback:logback-classic:1.2.6")
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:6.6")
-    runtimeOnly("org.webjars:swagger-ui:3.52.1")
+    runtimeOnly("org.webjars:swagger-ui:3.52.5")
     testImplementation(project(":libs:hops-common-test"))
     testImplementation("no.nav:kafka-embedded-env:2.8.0") {
         exclude("io.confluent", "kafka-schema-registry")
