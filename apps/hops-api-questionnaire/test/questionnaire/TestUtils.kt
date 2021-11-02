7258c4cd59c129258968a4b947e75c7e72a076d1
Robin <robin.tordly@nav.no>
2021-11-02 21:31:16 +0100

    set SO_REUSEADDR to false

--- apps/hops-api-questionnaire/test/questionnaire/TestUtils.kt
+++ apps/hops-api-questionnaire/test/questionnaire/TestUtils.kt
@@ -9,7 +9,10 @@ import org.hl7.fhir.r4.model.DomainResource
 import java.io.File
 import java.net.ServerSocket
 
-fun randomPort(): Int = ServerSocket(0).use(ServerSocket::getLocalPort)
+fun randomPort(): Int = ServerSocket(0).use {
+    it.reuseAddress = false // don't bind the same port (macOS)
+    it.localPort
+}
 
 fun resourceFile(path: String): String = object {}.javaClass.getResource(path)!!.readText()
 
