22dd0fd5628e0162ca4311ec9fdb3e87cc9f3c63
Eirik <eirikmyklebost+github@gmail.com>
2021-11-02 09:32:38 +0100

    added config

--- apps/hops-dialogmelding/main/dialogmelding/MQSender.kt
+++ apps/hops-dialogmelding/main/dialogmelding/MQSender.kt
@@ -7,7 +7,6 @@ import com.ibm.msg.client.jms.JmsConstants.JMS_IBM_ENCODING
 import com.ibm.msg.client.jms.JmsConstants.PASSWORD
 import com.ibm.msg.client.jms.JmsConstants.USERID
 import com.ibm.msg.client.jms.JmsFactoryFactory
-import com.ibm.msg.client.wmq.common.CommonConstants.USER_AUTHENTICATION_MQCSP
 import com.ibm.msg.client.wmq.common.CommonConstants.WMQ_APPLICATIONNAME
 import com.ibm.msg.client.wmq.common.CommonConstants.WMQ_CCSID
 import com.ibm.msg.client.wmq.common.CommonConstants.WMQ_CHANNEL
@@ -43,7 +42,6 @@ private fun createConnectionFactory(config: Config.MQ) =
         setIntProperty(WMQ_CCSID, UTF_8_WITH_PUA)
         setIntProperty(JMS_IBM_ENCODING, MQENC_NATIVE)
         setIntProperty(JMS_IBM_CHARACTER_SET, UTF_8_WITH_PUA)
-        setBooleanProperty(USER_AUTHENTICATION_MQCSP, true)
         setStringProperty(USERID, config.user)
         setStringProperty(PASSWORD, config.password)
     }
